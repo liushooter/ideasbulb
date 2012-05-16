2e76bf198b568ba657d594449508d637382b65ab
danjiang <danjiang5956@gmail.com>
2012-05-16 10:31:11 +0800

    make login with remember me work, fix #3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ideasbulb::Application.config.session_store :cookie_store, key: '_ideasbulb_session', :expire_after => 12.hours
+Ideasbulb::Application.config.session_store :cookie_store, key: '_ideasbulb_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
